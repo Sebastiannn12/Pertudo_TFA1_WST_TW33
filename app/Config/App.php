6a3ebffbb2b1d9920937c0d77d8a496a54078e72
Sebastiannn12 <francissebastianpertudo12@gmail.com>
2026-09-10 22:28:25 +0800

    Fix production base URL

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/Pertudo_WST_TFA1/';
+    public string $baseURL = 'https://sebastian-pos.onrender.com/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
